d69b0067483d9933d89cd0001c5f723beb526c40
carlos <caaz160782@gmail.com>
2024-09-15 13:19:31 -0600

    config router dom

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -12,7 +12,7 @@ const Layout = () => {
             </h1>
         </div>
     </header>
-    <main className="mt-10 mx-auto max-w-6xl">
+    <main className="mt-10 mx-auto max-w-6xl p-10 bg-white shadow">
         <Outlet/>
     </main>
     

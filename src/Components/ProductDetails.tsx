9f5cf0ab615edce39ece85165a140a1abfef9012
carlos <caaz160782@gmail.com>
2024-09-19 20:48:11 -0600

    usenavigate

---
+++ src/Components/ProductDetails.tsx
@@ -0,0 +1,42 @@
+import { /*Link,*/ useNavigate} from "react-router-dom"
+import { Product } from "../types"
+import { formatCurrency } from "../utils"
+
+type ProductDetailsProps={
+  product: Product
+}
+ 
+export default function ProductDetails({product}:ProductDetailsProps) {
+  
+  const navigate = useNavigate();
+
+  const isAvailable = product.availability
+
+  return (
+    <tr className="border-b ">
+    <td className="p-3 text-lg text-gray-800">
+      {product.name}
+    </td>
+    <td className="p-3 text-lg text-gray-800">
+      {formatCurrency(product.price)}
+    </td>
+    <td className="p-3 text-lg text-gray-800">
+      {isAvailable ?'Disponible':'No Disponible'}
+    </td>
+    <td className="p-3 text-lg text-gray-800 ">
+       <div className="flex gap-2 items-center">
+          {/* <Link 
+            className="bg-indigo-600 text-white rounded-lg w-full p-2 uppercase font-bold text-xs text-center"
+            to={`productos/${product.id}/edit`}
+          >
+             Editar</Link> */}
+          <button 
+            className="bg-indigo-600 text-white rounded-lg w-full p-2 uppercase font-bold text-xs text-center"
+            onClick={()=>navigate(`productos/${product.id}/edit`,{ state:{ product}})}
+          >
+             Editar</button>
+       </div>
+    </td>
+</tr> 
+  )
+}
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,7 +1,16 @@
-import { number, object,string } from "valibot"
+import { array, boolean, InferOutput, number, object,string } from "valibot"
 
 
 export const DraftProducSchema= object({
     name:string(),
     price: number()
 })
+
+export const ProducSchema= object({
+    id:number(),
+    name:string(),
+    price: number(),
+    availability:boolean()
+})
+export const ProductsSchema =array(ProducSchema)
+export type Product = InferOutput<typeof ProducSchema>
